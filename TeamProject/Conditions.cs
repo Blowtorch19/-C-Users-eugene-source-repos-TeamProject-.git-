f4913840bbbc0776020f728a6a1cc71736838a82
MariMaruMari <59061060+MariMaruMari@users.noreply.github.com>
2019-12-19 21:01:26 +0300

    Create Conditions.cs

---
+++ TeamProject/Conditions.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeamProject
+{
+    public class Conditions
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Concentration { get; set; }
+
+        public Conditions()
+        {
+
+        }
+
+
+    }
+}
