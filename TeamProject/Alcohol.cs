ad109faf24800e3fd61c2895a19bab769a4a6947
MariMaruMari <59061060+MariMaruMari@users.noreply.github.com>
2019-12-19 20:59:48 +0300

    Create Alcohol.cs

---
+++ TeamProject/Alcohol.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeamProject
+{
+    public class Alcohol
+    {
+        public string Name { get; set; }
+        public int AlcoholicBeverage { get; set; }
+
+        public Alcohol(string name, int alcoholicBeverage)
+        {
+            name = Name;
+            alcoholicBeverage = AlcoholicBeverage;
+        }
+
+        public Alcohol()
+        {
+            
+        }
+    }
+}
