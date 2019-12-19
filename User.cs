128198c34711e03388014bd09854087b98fb01b6
Blowtorch19 <58982141+Blowtorch19@users.noreply.github.com>
2019-12-19 20:29:01 +0300

    Add files via upload

---
+++ User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeamProject
+{
+    public class User
+    {
+        public int? Age { get; set; }
+        public double? Weight { get; set; }
+        public string Sex { get; set; }
+        public double? WidmarK { get; set; }
+        public string AlcoholType { get; set; }
+
+    }
+}
