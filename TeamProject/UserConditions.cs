640f7b9b55ae9ca19cb252cc4f2b35388c6dd6d6
AlexeyToday <59062165+AlexeyToday@users.noreply.github.com>
2019-12-19 21:20:06 +0300

    Update UserConditions.cs

--- TeamProject/UserConditions.cs
+++ TeamProject/UserConditions.cs
@@ -1,1 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
 
+namespace TeamProject
+{
+    public class UserConditions
+    {
+        public string Name { get; set; }
+        public double? Volume { get; set; }
+        public double? Hours { get; set; }
+
+        public UserConditions()
+        {
+
+        }
+    }
+}
